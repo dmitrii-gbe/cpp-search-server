537f09e83f265d6059990c8cc15864f2d4114ce4
dmitrii-gbe <115346210+dmitrii-gbe@users.noreply.github.com>
2023-01-09 14:26:12 +0300

    My first commit

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int y = 0;
+    int x = 0;
+    for (int i = 0; i <= 1000; ++i){
+        string s = to_string(i);
+        //cout << s << endl;
+        for (int g = 0; g < s.size(); ++g){
+            if (s[g] == '3'){
+                ++y;
+            }
+        }
+            if (y > 0){
+            ++x;
+            y = 0;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
+
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+// Напишите ответ здесь: 271
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
